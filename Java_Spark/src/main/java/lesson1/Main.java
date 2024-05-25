5040aa09006c4ea4767f40ba6f0383700f0cb204
mkoner <amzid@hotmail.fr>
2024-05-25 00:53:05 -0500

    Created SparkSeesion, a context, a parallelized RDD, few actions ant transformation

--- Java_Spark/src/main/java/lesson1/Main.java
+++ Java_Spark/src/main/java/lesson1/Main.java
@@ -3,17 +3,32 @@ package lesson1;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        SparkConf conf = new SparkConf().setAppName("Lesson1").setMaster("local[*]");
-        JavaSparkContext sc = new JavaSparkContext(conf);
-        List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
-        JavaRDD<Integer> myRdd = sc.parallelize(data);
-        myRdd.foreach(num -> System.out.println(num));
+
+        try (SparkSession session = SparkSession.builder()
+                .appName("Lesson1")
+                .master("local[*]").getOrCreate();
+             JavaSparkContext sc = new JavaSparkContext(session.sparkContext());) {
+            List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
+            JavaRDD<Integer> myRdd = sc.parallelize(data);
+            myRdd.foreach(num -> System.out.println(num));
+            System.out.println("Count: " + myRdd.count());
+            System.out.println("Number of partitions: " + myRdd.partitions().size());
+            System.out.println("Number of partitions: " + myRdd.getNumPartitions());
+
+            Integer max = myRdd.reduce(Integer::max);
+            System.out.println("Max: " + max);
+            Integer min = myRdd.reduce(Integer::min);
+            System.out.println("Min: " + min);
+            Integer sum = myRdd.reduce(Integer::sum);
+            System.out.println("Sum: " + sum);
+
+        }
     }
 }
