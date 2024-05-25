d0ec95a60d296384ee407b59aa51e9bdad2a2fe8
mkoner <amzid@hotmail.fr>
2024-05-25 18:02:26 -0500

    Added a Scanner so that the application will keep running while I run Spark Web UI

--- Java_Spark/src/main/java/lesson1/Main.java
+++ Java_Spark/src/main/java/lesson1/Main.java
@@ -7,13 +7,24 @@ import org.apache.spark.sql.SparkSession;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        try (SparkSession session = SparkSession.builder()
-                .appName("Lesson1")
-                .master("local[*]").getOrCreate();
+//        SparkConf conf = new SparkConf()
+//                .setAppName("MySparkApp")
+//                .setMaster("local[*]")
+//                .set("spark.ui.port", "4041");
+//        SparkSession session = SparkSession.builder()
+//                .config(conf).getOrCreate();
+
+
+
+        try (
+             SparkSession session = SparkSession.builder()
+                     .appName("Lesson1")
+                     .master("local[*]").getOrCreate();
              JavaSparkContext sc = new JavaSparkContext(session.sparkContext());
              ) {
             List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
@@ -29,6 +40,10 @@ public class Main {
             System.out.println("Min: " + min);
             Integer sum = myRdd.reduce(Integer::sum);
             System.out.println("Sum: " + sum);
+
+            try(Scanner scanner = new Scanner(System.in)){
+                scanner.nextLine();
+            }
 
         }
     }
