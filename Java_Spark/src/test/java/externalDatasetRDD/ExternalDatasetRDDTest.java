b80e31e45e3360bf135891924089a032bcc5c9ec
mkoner <amzid@hotmail.fr>
2024-06-09 16:12:51 -0500

    loading local text file into Spark RDD using MethodSource

--- Java_Spark/src/test/java/externalDatasetRDD/ExternalDatasetRDDTest.java
+++ Java_Spark/src/test/java/externalDatasetRDD/ExternalDatasetRDDTest.java
@@ -1,10 +1,16 @@
 package externalDatasetRDD;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
 
 public class ExternalDatasetRDDTest {
     private final SparkConf sparkConf = new SparkConf().setAppName("RDDExternalDatasetsTest").setMaster("local[*]");
@@ -25,5 +31,26 @@ public class ExternalDatasetRDDTest {
             myRdd.take(10).forEach(System.out::println);
             System.out.println("--------------------");
         }
+    }
+
+    @ParameterizedTest
+    @MethodSource("getFilePaths")
+    @DisplayName("Test loading local text file into Spark RDD using MethodSource")
+    void testLoadingLocalTextFileIntoSparkRDDUsingMethodSource(final String testFilePath) {
+        try (final JavaSparkContext sparkContext = new JavaSparkContext(sparkConf)) {
+            final JavaRDD<String> myRdd = sparkContext.textFile(testFilePath);
+
+            System.out.printf("Total lines in file %d%n", myRdd.count());
+            System.out.println("Printing first 10 lines~>");
+
+            myRdd.take(10).forEach(System.out::println);
+            System.out.println("--------------------");
+        }
+    }
+
+    private static Stream<Arguments> getFilePaths() {
+        return Stream.of(
+                Arguments.of(Path.of("src", "test", "resources", "abc.txt").toString()),
+                Arguments.of(Path.of("src", "test", "resources", "abc1.txt.gz").toString()));
     }
 }
