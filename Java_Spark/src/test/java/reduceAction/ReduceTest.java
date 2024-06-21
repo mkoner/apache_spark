17ed8ba9c83f9c76b66cabde79f82b84f3d8b6ad
mkoner <amzid@hotmail.fr>
2024-06-20 23:53:36 -0500

    Aggregate action on a parallelize RDD

--- Java_Spark/src/test/java/reduceAction/ReduceTest.java
+++ Java_Spark/src/test/java/reduceAction/ReduceTest.java
@@ -62,4 +62,24 @@ public class ReduceTest {
             System.out.printf("[Spark RDD Fold] time taken: %d ms%n%n", timeElapsed);
         }
     }
+
+    @Test
+    @DisplayName("Test aggregate() action using Spark RDD")
+    void testAggregateActionUsingSparkRDD() {
+        try (final var sparkContext = new JavaSparkContext(sparkConf)) {
+            final var myRdd = sparkContext.parallelize(data, 14);
+
+            final Instant start = Instant.now();
+            for (int i = 0; i < noOfIterations; i++) {
+                final var sum = myRdd.aggregate(0D, Double::sum, Double::sum);
+                final var max = myRdd.aggregate(0D, Double::sum, Double::max);
+                final var min = myRdd.aggregate(10D, Double::sum, Double::min);
+                System.out.println("[Spark RDD Aggregate] SUM: " + sum);
+                System.out.println("[Spark RDD Aggregate] MAX for all partitions: " + max);
+                System.out.println("[Spark RDD Aggregate] MIN for all partitions: " + min);
+            }
+            final long timeElapsed = (Duration.between(start, Instant.now()).toMillis()) / noOfIterations;
+            System.out.printf("[Spark RDD Aggregate] time taken: %d ms%n%n", timeElapsed);
+        }
+    }
 }
